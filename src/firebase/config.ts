4ec6bd25aff318d98c331814083891e986d95f1d
Venkat <vkongara050@gmail.com>
2025-12-25 06:36:00 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2368772135-c00a4",
   "appId": "1:981579822442:web:7b3f1e5c583c11d22ce113",
+  "storageBucket": "studio-2368772135-c00a4.appspot.com",
   "apiKey": "AIzaSyC6qrw6PfHrIDhfFJc-0R_EnvHauSsqYjU",
   "authDomain": "studio-2368772135-c00a4.firebaseapp.com",
   "measurementId": "",
